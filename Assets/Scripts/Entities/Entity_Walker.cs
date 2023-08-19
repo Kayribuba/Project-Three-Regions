056dc313dfb1f21226d5eaf578ad58d375ab29c1
kayra <khayridural@gmail.com>
2023-08-19 23:53:32 +0300

    walker_attack_state

--- Assets/Scripts/Entities/Entity_Walker.cs
+++ Assets/Scripts/Entities/Entity_Walker.cs
@@ -4,33 +4,90 @@ using UnityEngine;
 
 public class Entity_Walker : Entity
 {
+    enum WalkerBehaviour { Walking, Waiting, Attacking }
+
+
+    [SerializeField] LayerMask SightObstructionLayers = 1 << 6 | 1 << 7;
+    [SerializeField] GameObject WarnedEffect;
+    [SerializeField] Transform WarnedEffectPoint;
     [SerializeField] Transform downCheck;
     [SerializeField] Transform sideCheck;
+    [SerializeField] float attackCooldown = 1f;
+    [SerializeField] float sightRadius = 5f;
     [SerializeField] float downCheckRange = .5f;
     [SerializeField] Vector2 sideCheckSize = Vector2.one;
     [SerializeField] float pauseTime = 1f;
+    [SerializeField] float rememberTime = 1f;
+
+    GameObject Player;
 
     WalkerBehaviour currentBeh = WalkerBehaviour.Walking;
     Vector2 direction => isFacingLeft ? Vector2.left : Vector2.right;
     bool isFacingLeft;
 
+    float nextAttackTime = -1;
+    float forgetPlayerAt = -1;
+    bool isRememberingPlayer => forgetPlayerAt > Time.time;
+
+    public override void Start()
+    {
+        base.Start();
+
+        Player = GameManager.Instance.Player;
+    }
+
     internal override void UpdateBehaviour()
     {
+        TrySeePlayer();
+
         switch(currentBeh)
         {
             case WalkerBehaviour.Walking:
-                Walking();
+                Walk();
                 break;
             case WalkerBehaviour.Waiting:
-                Waiting();
+                Wait();
                 break;
             case WalkerBehaviour.Attacking:
-                Attacking();
+                Attack();
                 break;
         }
     }
 
-    void Walking()
+    bool TrySeePlayer()
+    {
+        if (Player == null)
+        {
+            Player = GameManager.Instance.Player;
+
+            if (Player == null) return false;
+        }
+
+        Vector2 playerPos = Player.transform.position;
+        Vector2 transformPos = transform.position;
+
+        if (Vector2.Distance(transformPos, playerPos) > sightRadius) return false;
+
+        Vector2 sightDir = playerPos - transformPos;
+        RaycastHit2D hit = Physics2D.Raycast(transformPos, sightDir, sightRadius, SightObstructionLayers);
+
+        if (hit.collider == null) return false;
+
+        if (hit.collider.gameObject.tag == "Player")
+        {
+            if (isRememberingPlayer == false)//First time entering
+            {
+                SwitchBehaviour(WalkerBehaviour.Attacking);
+            }
+
+            forgetPlayerAt = Time.time + rememberTime;
+            return true;
+        }
+
+        return false;
+    }
+
+    void Walk()
     {
         Debug.Log("walk");
 
@@ -54,7 +111,7 @@ public class Entity_Walker : Entity
     }
 
     float waitExitTime = float.MaxValue;
-    void Waiting()
+    void Wait()
     {
         if(waitExitTime <= Time.time)
         {
@@ -62,9 +119,26 @@ public class Entity_Walker : Entity
             SwitchBehaviour(WalkerBehaviour.Walking);
         }
     }
-    void Attacking()
+    void Attack()
     {
-        Debug.Log("attack");
+        Debug.Log("Attacking");
+
+        if (nextAttackTime <= Time.time)
+        {
+            //attack here
+            /*GameObject instProj = Instantiate(_projectile, _barrel.transform.position, Quaternion.identity);
+            if (instProj.TryGetComponent(out Projectile outProj))
+            {
+                //outProj.Initalize(direction, );
+            }*/
+
+            nextAttackTime = Time.time + attackCooldown;
+        }
+
+        if (isRememberingPlayer == false)
+        {
+            SwitchBehaviour(WalkerBehaviour.Walking);
+        }
     }
 
     void SwitchBehaviour(WalkerBehaviour switchTo)
@@ -75,18 +149,25 @@ public class Entity_Walker : Entity
 
                 break;
             case WalkerBehaviour.Waiting:
-                rb.velocity = rb.velocity * new Vector2(0, 1);
                 waitExitTime = Time.time + pauseTime;
                 break;
             case WalkerBehaviour.Attacking:
-
+                if (WarnedEffect != null)
+                {
+                    if (WarnedEffectPoint != null)
+                    { Instantiate(WarnedEffect, WarnedEffectPoint.transform.position, Quaternion.identity, WarnedEffectPoint.transform); }
+                    else
+                    {
+                        Instantiate(WarnedEffect, transform.position, Quaternion.identity, transform);
+                    }
+                }
                 break;
         }
 
         switch (currentBeh)//Exiting behaviour
         {
             case WalkerBehaviour.Walking:
-                
+                rb.velocity = rb.velocity * new Vector2(0, 1);
                 break;
             case WalkerBehaviour.Waiting:
                 waitExitTime = float.MaxValue;
@@ -116,7 +197,7 @@ public class Entity_Walker : Entity
     {
         Gizmos.DrawLine(downCheck.position, (Vector2)downCheck.position + Vector2.down * downCheckRange);
         Gizmos.DrawWireCube(sideCheck.position, sideCheckSize);
-    }
 
-    enum WalkerBehaviour { Walking, Waiting, Attacking}
+        Gizmos.DrawWireSphere(transform.position, sightRadius);
+    }
 }
